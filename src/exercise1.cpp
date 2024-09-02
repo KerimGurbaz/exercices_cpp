a1b42f8166f6833e1cedb98ec7d638d0dd2bc6a6
Kerim <kerim.grbz.01@gmail.com>
2024-09-02 12:34:33 +0200

    premier commit via clion

--- src/exercise1.cpp
+++ src/exercise1.cpp
@@ -1,9 +1,23 @@
-//
-// Created by Krm on 02/09/2024.
-//
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int n, p;
+
+    // İlk kısım
+    n = p = 0;
+    while (n < 5)
+        n += 2;
+    p++;
+    cout << "A: n = " << n << " p = " << p << "\n";
+
+    // İkinci kısım
+    n = p = 0;
+    while (n < 5) {
+        n += 2;
+        p++;
+    }
+    cout << "B: n = " << n << " p = " << p << "\n";
+
     return 0;
 }
