84f26c545ecefb68d9ad2e0897d0c10f8740be24
Kerim <kerim.grbz.01@gmail.com>
2024-09-15 11:30:18 +0200

    some practice problems for the c++ exam and solutions for the problem. plus enum structure

--- src/exercise1.cpp
+++ src/exercise1.cpp
@@ -610,40 +610,93 @@ inline int fct(char c, int n) {
  */
 
 
+//int main()
+//{
+//    int n = 4, k = 2;
+//
+//    cout << ++n << endl;
+//    cout << n << endl;
+//    cout << n++ << endl;
+//    cout << n << endl;
+//    cout << -n << endl;
+//    cout << n << endl;
+//
+//    cout << --n << endl;
+//    cout << n << endl;
+//
+//    cout << n-- << endl;
+//    cout << n << endl;
+//
+//    cout << n + k << endl;
+//    cout << n << endl;
+//    cout << k << endl;
+//    cout << n << k << endl;
+//
+//    cout << n << endl;
+//    cout << " " << n << endl;
+//    cout << " n" << endl;
+//    cout << "\n" << endl;
+//
+//    cout << " n * n = "; //CAREFUL!
+//    cout << n * n << endl;
+//    cout << 'n' << endl;
+//    return 0;
+//
+//}
+
+/*
+ * int main()
+{
+    int n = 3;
+    while (n >= 0)
+    {
+        cout << n * n << endl;
+        --n;
+    }
+    cout << n << endl;
+
+    while (n < 4)
+        cout << ++n << endl;
+    cout << n << endl;
+
+    while (n >= 0)
+        cout << (n /= 2) << endl;
+
+    return 0;
+
+    }
+
+
+//operations logique
+int main() {
+    int n;
+    cout << (n = 4) << endl;
+    cout << (n == 4) << endl;
+    cout << (n > 3) << endl;
+    cout << (n < 4) << endl;
+    cout << (n = 0) << endl;
+    cout << (n == 0) << endl;
+    cout << (n > 0) << endl;
+    cout << (n && 4) << endl;
+    cout << (n || 4) << endl;
+    cout << (!n) << endl;;
+
+
+    return 0;
+}
+
+ */
 
 int main()
 {
-    int n = 4, k = 2;
-
-    cout << ++n << endl;
-    cout << n << endl;
-    cout << n++ << endl;
-    cout << n << endl;
-    cout << -n << endl;
-    cout << n << endl;
-
-    cout << --n << endl;
-    cout << n << endl;
-
-    cout << n-- << endl;
-    cout << n << endl;
-
-    cout << n + k << endl;
-    cout << n << endl;
-    cout << k << endl;
-    cout << n << k << endl;
-
-    cout << n << endl;
-    cout << " " << n << endl;
-    cout << " n" << endl;
-    cout << "\n" << endl;
-
-    cout << " n * n = "; //CAREFUL!
-    cout << n * n << endl;
-    cout << 'n' << endl;
+    enum color_type {red, orange, yellow, green, blue, violet};
+    color_type shirt, pants;
+    shirt = red;
+    pants = blue;
+    cout << shirt << " " << pants << endl;
     return 0;
-
 }
+
 
 
 
