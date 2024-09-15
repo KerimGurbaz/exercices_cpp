e6d318f11f51e76ca3914b7f08b8abafaf2a5a70
Kerim <kerim.grbz.01@gmail.com>
2024-09-15 09:56:32 +0200

    some practice problems for the c++ exam and solutions for the problem.

--- src/exercise1.cpp
+++ src/exercise1.cpp
@@ -581,11 +581,6 @@ int main() {
 
     cout<< "Apres : "<<n<< endl;
 }
-
-
-
-
- */
 inline int fct(char c, int n);
 
 int main() {
@@ -610,6 +605,45 @@ inline int fct(char c, int n) {
     return res;
 }
 
+
+
+ */
+
+
+
+int main()
+{
+    int n = 4, k = 2;
+
+    cout << ++n << endl;
+    cout << n << endl;
+    cout << n++ << endl;
+    cout << n << endl;
+    cout << -n << endl;
+    cout << n << endl;
+
+    cout << --n << endl;
+    cout << n << endl;
+
+    cout << n-- << endl;
+    cout << n << endl;
+
+    cout << n + k << endl;
+    cout << n << endl;
+    cout << k << endl;
+    cout << n << k << endl;
+
+    cout << n << endl;
+    cout << " " << n << endl;
+    cout << " n" << endl;
+    cout << "\n" << endl;
+
+    cout << " n * n = "; //CAREFUL!
+    cout << n * n << endl;
+    cout << 'n' << endl;
+    return 0;
+
+}
 
 
 
