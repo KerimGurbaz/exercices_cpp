9a07389ecc9ea5646e5dcc65164833d6cf2e272e
Kerim <kerim.grbz.01@gmail.com>
2024-09-16 12:27:40 +0200

    operations logique include cin, cin.getline() + v-2

--- src/exercise1.cpp
+++ src/exercise1.cpp
@@ -777,6 +777,16 @@ int main() {
 }
  */
 
+int main() {
+    const int LENGTH = 21;
+    char message[LENGTH];
+    cout << "Enter a sentence on the line below." << endl;
+    cin.getline(message, LENGTH, '\n');
+    cout << message << endl;
+
+    return 0;
+}
+
 
 
 
