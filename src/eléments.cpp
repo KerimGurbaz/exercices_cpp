33b37ae4c1594f8d68f3688f65742ba63d146844
Kerim <kerim.grbz.01@gmail.com>
2024-09-20 12:45:11 +0200

    references and pointer

--- src/eléments.cpp
+++ src/eléments.cpp
@@ -11,9 +11,7 @@ int main() {
     cout << "Fin de programme " << endl;
 
     return EXIT_SUCCESS;
-}
- */
-#include <iostream>
+}#include <iostream>
 #include <cstdlib>
 
 #include <iostream>
@@ -28,3 +26,60 @@ int main() {
 
 }
 
+#include <iostream>
+#include <cmath>
+using namespace std;
+int abs(int x){
+    cout<< x<<endl;
+  return (x < 0 )? -x : x;
+}
+
+float sqr(float x) {
+
+    return x*x;
+}
+void myFunction() {
+    // corps de la function
+}
+int main() {
+
+    int i = 5 ; // une définition
+    // int i; => declaration;
+    int result = abs(-100);
+    float result2 = sqr(2.2);
+    cout<<result<< endl;
+    cout<<result2 << endl;
+    int x =25;// initialisation de x avec la valeur 0;
+
+    void(*f)()=&myFunction;
+
+    struct Point {double x; double y;};
+
+    struct Thing;
+
+    //
+
+return 0;
+//int abs(int x); // ceci une déclaration(elle indique qu'il existe une fonction appelp abs qui prend un rntier x en paramètre et retourne un entier.
+
+//float sqr(float x); il s'agit d'une déclaration.
+}
+
+ */
+#include <iostream>
+using namespace std;
+
+
+#include <iostream>
+    using namespace std;
+
+    int main() {
+        char c = 'A';
+        char* ptr = &c;
+
+        cout << "Value of c: " << c << endl;                      // 'A'
+        cout << "Address stored in ptr (interpreted as string): " << ptr << endl;
+        cout << "Dereferenced value of ptr: " << *ptr << endl;    // 'A' yazdırılır.
+
+    return 0;
+}
