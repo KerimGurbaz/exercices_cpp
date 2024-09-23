a042bdf567b40e947c5a212d20f1dbcb6f33b0e2
Kerim <kerim.grbz.01@gmail.com>
2024-09-23 19:26:10 +0200

    references and pointer

--- src/eléments.cpp
+++ src/eléments.cpp
@@ -383,6 +383,19 @@ int main() {
 #include <string>
 using namespace std;
 
+int main() {
+    int n(7);
+
+    if(n<10)
+        if(n>0)
+            cout<<"The number is positive. "<< endl;
+    else
+        cout<< "The number is ----"<< endl;
+
+
+    return 0;
+}
+
 
 
 
